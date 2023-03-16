af21806d10b871074be519a71a30f04ea1ea3080
root <root@DESKTOP-O0HFKNQ.localdomain>
2023-03-16 02:23:15 +0100

    Add default route2

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const QRCode = require('qrcode');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Bienvenue sur la page d\'accueil!');
+app.get('/ma-route', (req, res) => {
+  res.send('Bienvenue sur ma route personnalisée!');
+});
+
+app.get('/qr-code', async (req, res) => {
+  try {
+    const url = 'https://example.com'; // Remplacez par l'URL souhaitée
+    const qrCodeImage = await QRCode.toDataURL(url);
+    res.send(`<img src="${qrCodeImage}" alt="QR Code" />`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erreur lors de la génération du QR Code');
+  }
 });
 
 const port = process.env.PORT || 3000;
