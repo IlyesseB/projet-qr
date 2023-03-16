806efe48240e4a46251e26d185ad0e26d710e06d
root <root@DESKTOP-O0HFKNQ.localdomain>
2023-03-16 02:04:01 +0100

    Serve static files with Express

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Ajoutez cette ligne pour servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
