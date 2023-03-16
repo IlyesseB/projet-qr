6e8e116a1b66f371bf241df6f86ab56a6d65e0c9
root <root@DESKTOP-O0HFKNQ.localdomain>
2023-03-16 01:54:01 +0100

    modif de tout les fichier

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`AR.js QR code generator listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
