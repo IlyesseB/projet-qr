bc0f154241d34a9bf43d7b192b185e381b0e83c8
root <root@DESKTOP-O0HFKNQ.localdomain>
2023-03-16 02:50:29 +0100

    Mise à jour de l'application

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/ma-route', (req, res) => {
   res.send('Bienvenue sur ma route personnalisée!');
