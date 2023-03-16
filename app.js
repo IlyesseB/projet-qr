32dc23a1af5be2aae8c264d30c954468effd65dd
root <root@DESKTOP-O0HFKNQ.localdomain>
2023-03-16 02:06:51 +0100

    Mise à jour de app.js pour servir les fichiers statiques

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Ajoutez cette ligne pour servir les fichiers statiques
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
