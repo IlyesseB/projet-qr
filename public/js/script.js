8da638e28779c69f121f1ee4cbfd9487395f036a
root <root@Ilyesse>
2023-03-24 09:30:38 +0100

    modif pour 2 qr code

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const qrCodeElement = document.getElementById('qrcode');
-  const url = 'https://www.ilyessebouraghda.fr/ar.html'; 
+document.addEventListener("DOMContentLoaded", function () {
+  const qrCodeElement1 = document.getElementById("qrcode1");
+  const qrCodeElement2 = document.getElementById("qrcode2");
+  const url1 = "https://your-domain.com/ar.html?modelUrl=models/model1/scene.gltf";
+  const url2 = "https://your-domain.com/ar.html?modelUrl=models/model2/scene.gltf";
 
-  new QRCode(qrCodeElement, {
-    text: url,
+  new QRCode(qrCodeElement1, {
+    text: url1,
     width: 256,
     height: 256,
-    colorDark: '#000000',
-    colorLight: '#ffffff',
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H,
+  });
+
+  new QRCode(qrCodeElement2, {
+    text: url2,
+    width: 256,
+    height: 256,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H,
   });
 });
